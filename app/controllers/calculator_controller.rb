a2999fc8fb04095db679e50079b3e9304dd61044
Jorge Yagüe París <jorgeyp@gmail.com>
2015-02-12 12:32:35 +0100

    Add subtract, multiply, divide methods :heavy_minus_sign: :heavy_multiplication_x: :heavy_division_sign:

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -3,10 +3,34 @@ class CalculatorController < ApplicationController
 
   # Add
   soap_action "add",
-              :args => { :a => :integer, :b => :integer},
-              :return => :integer
+              :args => { :a => :double, :b => :double},
+              :return => :double
   def add
     render :soap => (params[:a] + params[:b])
+  end
+
+  # Subtract
+  soap_action "subtract",
+              :args => { :a => :double, :b => :double},
+              :return => :double
+  def subtract
+    render :soap => (params[:a] - params[:b])
+  end
+
+  # Multiply
+  soap_action "multiply",
+              :args => { :a => :double, :b => :double},
+              :return => :double
+  def multiply
+    render :soap => (params[:a] * params[:b])
+  end
+
+  # Divide
+  soap_action "divide",
+              :args => { :a => :double, :b => :double},
+              :return => :double
+  def divide
+    render :soap => (params[:a] / params[:b])
   end
 
 end
